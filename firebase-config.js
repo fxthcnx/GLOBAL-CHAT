b5c9c24b22860ea82e112e9b9f6f5ea644cc86c0
fathona <fathonaj04@gmail.com>
2019-09-11 17:51:31 +0800

    copy pasted from video
    functions for chats and chat box and main input (where you type yr msges)

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDRZL9bc65HLOuUtiftSdt9zTAqEu9IF7Y",
+    authDomain: "global-chat-live-ba698.firebaseapp.com",
+    databaseURL: "https://global-chat-live-ba698.firebaseio.com",
+    projectId: "global-chat-live-ba698",
+    storageBucket: "",
+    messagingSenderId: "672778302939",
+    appId: "1:672778302939:web:c059b1620901794c30e3fe"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase.firestore();
